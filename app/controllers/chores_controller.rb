baf73d831a41cf76b1d72c00e06cddc1994d3194
Tyler Nguyen <tynguyen06@gmail.com>
2016-04-26 18:52:11 -0700

    Added google api

---
+++ app/controllers/chores_controller.rb
@@ -0,0 +1,27 @@
+class ChoresController < ApplicationController
+  def index
+    # @chores = Chore.all
+    # @hash = Gmaps4rails.build_markers(@chores) do |chore, marker|
+    #   marker.lat chore.client.address
+    #   marker.lng chore.client.address
+    # end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
